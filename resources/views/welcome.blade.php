f1e6a4e534f9f609d56f85c6b97b423bbfbfc5a9
andrew <iniandrewrp@gmail.com>
2023-06-02 15:07:49 +0700

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,30 +1,43 @@
 @extends('layouts.skeleton')
 
-@push('styles')
-    @vite('resources/css/style.css')
-@endpush
-
 @section('app')
-    <section id="home">
-        <div class="hero">
-            <div class="hero-header">
-                <p class="tagline text-success"># 1204200046</p>
-                <div class="hero-title">
-                    <h1>Chrisdion Andrew</h1>
+    <div class="bg-light mt-5" id="main">
+        <div class="container py-5 px-1">
+            <div class="row">
+                <div class="col-md-5 order-md-2">
+                    <img
+                        class="img-fluid shadow-lg rounded-3"
+                        src="{{ Vite::asset('resources/images/deon.jpg') }}"
+                    />
                 </div>
-                <div class="hero-subtitle">
-                    <p class="text-subtitle text-muted">A Web Developer with experience in building applications. Strong engineering professional skilled in
+                <div class="col-md-7 order-md-1">
+                    <h1 class="mt-4 display-3">
+                        Chrisdion Andrew
+                    </h1>
+                    <p class="text-muted"># 1204200046</p>
+                    <p class="fs-5 mt-3">
+                        A Web Developer with experience in building applications. Strong engineering professional skilled in
                         Web Development and PHP using a variety of frameworks, Back-End Web Development, as well as
                         other common programming languages such as Java, Kotlin, and Python. Also have basic knowledge
                         of the Linux OS. Have experience in working in remote team as well as communicating with clients or
                         customers.
                     </p>
+                    <div class="row">
+                        <div class="d-flex flex-column flex-md-row">
+                            <a
+                                href="{{ route('barang.index') }}"
+                                type="button"
+                                class="btn btn-dark text-white btn-lg mb-3 me-md-3 px-4 py-3"
+                            >
+                                Masuk ke Aplikasi
+                            </a>
+                        </div>
+                    </div>
                 </div>
-                <a href="{{ route('barang.index') }}" class="btn btn-dark rounded-4 px-4">Masuk ke Aplikasi</a>
             </div>
-            <img src="{{ Vite::asset('resources/images/deon.jpg') }}" class="rounded-circle shadow-lg w-25 h-auto" alt="">
         </div>
-        <div class="circle"></div>
-        <!-- Hero -->
-        </section>
+        <div>
+            <img src="{{ Vite::asset('resources/images/wave.svg') }}" class="img-fluid" alt="">
+        </div>
+    </div>
 @endsection
